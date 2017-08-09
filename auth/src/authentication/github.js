a80827ade763b18e4cbc14c9eec3c0e8d718a685
acharotariya <acharotariya@officebrain.com>
2017-08-09 20:16:23 +0530

    updated github api

--- auth/src/authentication/github.js
+++ auth/src/authentication/github.js
@@ -3,19 +3,7 @@ const microAuthGithub = require('microauth-github');
 const authGit = require('./authentication');
 const redirect = require('micro-redirect')
 const index = require('../../index')
- const User = require('../models/user');
-/*
-const { gitclientId,gitclientSecret,gitcallbackUrl,gitpath,gitscope } = require('../social-config');
-
-
-const options = {
-  clientId: gitclientId,
-  clientSecret: gitclientSecret,
-  callbackUrl: gitcallbackUrl,
-  path: gitpath,
-  scope: gitscope
-};
-*/
+const User = require('../models/user');
 const githubAuth = microAuthGithub(index.options);
 
 module.exports.github = githubAuth(async (req, res, auth) => {
@@ -23,31 +11,49 @@ module.exports.github = githubAuth(async (req, res, auth) => {
    let provider = auth.result.provider
    let fullname = auth.result.info.name
    let access_token = auth.result.accessToken
-   let data_length = await User.find({userid: id});
-   if(data_length.length !== 0){
-     throw createError(401, 'user already exist');
-   }else{
-     let user = new User({ aboutme:null, fullname:fullname, firstname:null, lastname:null, email:null, password:null, dob:null, role:null,signup_type:null,image_name:null,image_url:null,forget_token_created_at:null,provider:provider,access_token:access_token, userid : id});
-     user = user.save();
-     console.log(auth);
-     console.log("Access_Token " + access_token)
-   }
-  if (!auth) {
-    return send(res, 404, 'Not Found');
-  }
 
-  if (auth.err) {
-    // Error handler
-    return send(res, 403, 'Forbidden');
-  }
+    let data_length = await User.find({ social_uid: id });
+    let data = data_length[0];
 
-    token = authGit.sociallogin(auth);
-    console.log("token" + token.token);
-    const statusCode = 302
-    const location = index.redirect_app_url+'?token='+token.token
-    console.log(location);
-    redirect(res, statusCode, location)
 
-//send(res, 200, authGit.sociallogin(auth));
+     if (!auth) {
+       return send(res, 404, 'Not Found');
+     }
+
+     if (auth.err){
+       // Error handler
+       //fail url
+       return send(res, 403, 'Forbidden');
+     }
+
+   // console.log("googletoken",token);
+     if( data_length.length == 0){
+       let user = new User({ aboutme:null, fullname:fullname, firstname:null, lastname:null, email:null, password:null, dob:null, role:null,signup_type:null,image_name:null,image_url:null,forget_token_created_at:null,provider:provider,access_token:access_token,isEmailConfirm:0,social_uid:id});
+         user.save(function(err){
+           if(err)
+           {
+             console.log(err);
+           }
+           else{
+             //console.log(user._id);
+             let ob_id = user._id;
+             const statusCode = 302
+             const location = index.redirect_app_url+'?ob_id='+ob_id
+             redirect(res, statusCode, location)
+           }
+         });
+       }else if(data.isEmailConfirm == 1){
+         let ob_id = data._id;
+         token = authGit.sociallogin(ob_id);
+         const logintoken  = token.logintoken;
+         const statusCode = 302
+         const location = index.redirect_app_url+'?token='+logintoken
+         redirect(res, statusCode, location)
+       }else{
+         let ob_id = data._id;
+         const statusCode = 302
+         const location = index.redirect_app_url+'?ob_id='+ob_id
+         redirect(res, statusCode, location)
+       }
 
 });
