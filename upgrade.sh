aa9ec654144aad0d92d9206576fd5b3c04ecab1e
Ajay Charotariya <acharotariya@officebrain.com>
2018-01-12 17:32:15 +0530

    added upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,23 +1,96 @@
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
--X POST \
--H 'Accept: application/json' \
--H 'Content-Type: application/json' \
--d '{
-  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/authentication_auth_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3001:3001/tcp"],"version": "472d2d0a-f357-4dd7-9ecf-0daa7604ada9","environment": {"MONGODB": "mongodb://obdev2:123456789@ds125966-a0.mlab.com:25966/closeoutpromo","SECRET": "abcdefgabcdefg"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3001,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/$SERVICE_ID_AUTH?action=upgrade
+# curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+# -X POST \
+# -H 'Accept: application/json' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#   "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/authentication_auth_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3001:3001/tcp"],"version": "472d2d0a-f357-4dd7-9ecf-0daa7604ada9","environment": {"MONGODB": "mongodb://obdev2:123456789@ds125966-a0.mlab.com:25966/closeoutpromo","SECRET": "abcdefgabcdefg"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3001,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+# http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/$SERVICE_ID_AUTH?action=upgrade
+#
+# curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+# -X POST \
+# -H 'Accept: application/json' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#   "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/authentication_ldap_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3000:3000/tcp"],"version": "fbcb0cf8-22f5-4134-baf9-55bc314f2f5a","environment": {"LDAPURL":"ldap://openldap-authentication","ADMINPASS":"123456","USERPASS":"123" },"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3000,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+# 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s99?action=upgrade'
+#
+# curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+# -X POST \
+# -H 'Accept: application/json' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#   "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/authentication_user_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3002:3002/tcp"],"version": "fbcb0cf8-22f5-4134-baf9-55bc314f2f5a","environment": {"MONGODB": "mongodb://obdev2:123456789@ds125966-a0.mlab.com:25966/closeoutpromo","SECRET": "abcdefgabcdefg"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3002,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+# 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s106?action=upgrade'
+
+
+
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    {
+    echo "call $TRAVIS_BRANCH branch"
+    ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Production" | jq '.data[].id' | tr -d '"'`
+    echo $ENV_ID
+    USERNAME="$DOCKER_USERNAME_FLOWZ";
+    TAG="latest";
+    MONGODB="$MONGODB_MASTER";
+    SECRET="$SECRET_MASTER";
+    DOMAINKEY="$DOMAINKEY_MASTER";
+  }
+elif [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="dev";
+      MONGODB="$MONGODB_DEVELOP";
+      SECRET="$SECRET_DEVELOP";
+      DOMAINKEY="$DOMAINKEY_DEVELOP";
+    }
+else
+  {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="qa";
+      MONGODB="$MONGODB_QA";
+      SECRET="$SECRET_QA";
+      DOMAINKEY="$DOMAINKEY_QA";
+  }
+fi
+
+SERVICE_ID_AUTH=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=auth-authentication-flowz" | jq '.data[].id' | tr -d '"'`
+echo $SERVICE_ID_AUTH
+
+SERVICE_ID_LDAP=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=ldap-authentication-flowz" | jq '.data[].id' | tr -d '"'`
+echo $SERVICE_ID_LDAP
+
+SERVICE_ID_USER=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=user-authentication-flowz" | jq '.data[].id' | tr -d '"'`
+echo $SERVICE_ID_USER
 
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:obdev/authentication_ldap_flowz:dev","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3000:3000/tcp"],"version": "fbcb0cf8-22f5-4134-baf9-55bc314f2f5a","environment": {"LDAPURL":"ldap://openldap-authentication","ADMINPASS":"123456","USERPASS":"123" }}},"toServiceStrategy":null}' \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s99?action=upgrade'
+  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/authentication_auth_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3001:3001/tcp"],"environment": {"MONGODB": "'"$MONGODB"'","SECRET": "'"$SECRET"'","DOMAINKEY":"'"$DOMAINKEY"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3001,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_AUTH?action=upgrade
 
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:obdev/authentication_user_flowz:dev","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3002:3002/tcp"],"version": "fbcb0cf8-22f5-4134-baf9-55bc314f2f5a","environment": {"MONGODB": "mongodb://obdev2:123456789@ds125966-a0.mlab.com:25966/closeoutpromo","SECRET": "abcdefgabcdefg"}}},"toServiceStrategy":null}' \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s106?action=upgrade'
+  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/authentication_ldap_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3000:3000/tcp"],"environment": {"LDAPURL":"'"$LDAPURL"'","ADMINPASS":"'"$ADMINPASS"'","USERPASS":"'"$USERPASS"'" },"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3000,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_LDAP?action=upgrade
+
+curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{
+  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/authentication_user_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3002:3002/tcp"],"environment": {"MONGODB": "'"$MONGODB"'","SECRET": "'"$SECRET"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3002,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_USER?action=upgrade
