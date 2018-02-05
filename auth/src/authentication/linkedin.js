7da90c56ed5f6029a2dca5577a88d10bbb6962e4
Ajay Charotariya <acharotariya@officebrain.com>
2018-02-05 19:16:03 +0530

    isActive feature added in social login

--- auth/src/authentication/linkedin.js
+++ auth/src/authentication/linkedin.js
@@ -35,7 +35,7 @@ module.exports.linkedin = linkedinAuth(async (req, res, auth) => {
 
   // console.log("googletoken",token);
   if (data_length.length == 0) {
-    let user = new User({ aboutme: null, fullname: fullname, firstname: firstname, lastname: lastname, email: email, password: null, dob: null, role: null, signup_type: null, image_name: null, image_url: publicProfileUrl, forget_token_created_at: null, provider: provider, access_token: access_token, isEmailConfirm: 0, social_uid: id });
+    let user = new User({ aboutme: null, fullname: fullname, firstname: firstname, lastname: lastname, email: email, password: null, dob: null, role: null, signup_type: null, image_name: null, image_url: publicProfileUrl, forget_token_created_at: null, provider: provider, access_token: access_token, isEmailConfirm: 0, social_uid: id, isActive:1 });
     user.save(function (err) {
       if (err) {
         console.log(err);
